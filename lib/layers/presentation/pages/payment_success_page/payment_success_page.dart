5fd4894c08e05c83a2ba9056d8742bc1ba75cbdb
Arllain <arllain@gmail.com>
2022-10-28 20:53:16 -0300

    feat: internationalization on payment success page

--- lib/layers/presentation/pages/payment_success_page/payment_success_page.dart
+++ lib/layers/presentation/pages/payment_success_page/payment_success_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
 import 'package:smart_menu_app/layers/presentation/pages/scan_qrcode_page/scan_qrcode_page.dart';
 import 'package:smart_menu_app/layers/presentation/utils/app_styles.dart';
 
@@ -32,13 +33,14 @@ class PaymentSuccessPage extends StatelessWidget {
               width: 150,
             ),
             const SizedBox(height: 20),
-            const Text(
-              'Payment Success!',
-              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+            Text(
+              'payment_success'.i18n(),
+              style:
+                  const TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
             Text(
-              'Thank you for your payment!',
+              'thank_you_for_your_payment'.i18n(),
               style: Styles.textStyle
                   .copyWith(color: Styles.greyColor, fontSize: 12),
             ),
@@ -61,7 +63,7 @@ class PaymentSuccessPage extends StatelessWidget {
               color: Styles.buttonColor,
               child: Center(
                 child: Text(
-                  'Continue',
+                  'continue'.i18n(),
                   style: Styles.headLineStyle2
                       .copyWith(color: Colors.white, fontSize: 18),
                 ),
