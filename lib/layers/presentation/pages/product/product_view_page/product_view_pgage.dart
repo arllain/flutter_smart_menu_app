9896f47981709387696a13fa9605541803df6fc6
Arllain <arllain@gmail.com>
2022-10-28 19:59:00 -0300

    reafacto: product view page

--- lib/layers/presentation/pages/product/product_view_page/product_view_pgage.dart
+++ lib/layers/presentation/pages/product/product_view_page/product_view_pgage.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:localization/localization.dart';
 import 'package:smart_menu_app/layers/domain/entities/product/product_entity.dart';
+import 'package:smart_menu_app/layers/presentation/pages/cart_page/bloc/cart_bloc.dart';
 import 'package:smart_menu_app/layers/presentation/utils/app_layout.dart';
 import 'package:smart_menu_app/layers/presentation/utils/app_styles.dart';
 
@@ -11,143 +14,153 @@ class ProductViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: CustomScrollView(slivers: [
-        SliverAppBar(
-          expandedHeight: MediaQuery.of(context).size.height * 0.6,
-          elevation: 0,
-          snap: true,
-          floating: true,
-          stretch: true,
-          backgroundColor: Colors.grey.shade50,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: const [
-              StretchMode.zoomBackground,
-            ],
-            background: CachedNetworkImage(
-              imageUrl: product.imageURL,
-              imageBuilder: (context, imageProvider) => Container(
-                height: AppLayout.getHeight(150),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Styles.primaryColor,
-                  image: DecorationImage(
-                    image: imageProvider,
-                    fit: BoxFit.cover,
+      backgroundColor: Colors.black,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: MediaQuery.of(context).size.height * 0.6,
+            elevation: 0,
+            snap: true,
+            floating: true,
+            stretch: true,
+            backgroundColor: Colors.red,
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                color: Colors.black,
+                icon: const Icon(Icons.arrow_back)),
+            flexibleSpace: FlexibleSpaceBar(
+              stretchModes: const [
+                StretchMode.zoomBackground,
+              ],
+              background: CachedNetworkImage(
+                imageUrl: product.imageURL,
+                imageBuilder: (context, imageProvider) => Container(
+                  height: AppLayout.getHeight(150),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Styles.primaryColor,
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              placeholder: (context, url) => const Center(
-                child: CircularProgressIndicator(),
+                placeholder: (context, url) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
               ),
             ),
-          ),
-          bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(45),
-              child: Transform.translate(
-                offset: const Offset(0, 1),
-                child: Container(
-                  height: 45,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
+            bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(45),
+                child: Transform.translate(
+                  offset: const Offset(0, 1),
+                  child: Container(
+                    height: 45,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: Center(
+                      child: Container(
+                        width: 50,
+                        height: 8,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade300,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
                     ),
                   ),
-                  child: Center(
-                      child: Container(
-                    width: 50,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade300,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  )),
-                ),
-              )),
-        ),
-        SliverList(
-            delegate: SliverChildListDelegate([
-          Container(
-              height: MediaQuery.of(context).size.height * 0.55,
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+                )),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.55,
+                  color: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Column(
+                      Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.name,
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                product.category.name,
+                                style: TextStyle(
+                                  color: Colors.orange.shade400,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
                           Text(
-                            product.name,
+                            "\$ ${product.price}",
                             style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            product.category.name,
-                            style: TextStyle(
-                              color: Colors.orange.shade400,
-                              fontSize: 14,
-                            ),
+                                color: Colors.black, fontSize: 16),
                           ),
                         ],
                       ),
+                      const SizedBox(height: 20),
                       Text(
-                        "\$ ${product.price}",
-                        style:
-                            const TextStyle(color: Colors.black, fontSize: 16),
+                        product.description,
+                        style: TextStyle(
+                          height: 1.5,
+                          color: Colors.grey.shade800,
+                          fontSize: 15,
+                        ),
                       ),
+                      const SizedBox(height: 10),
+                      const SizedBox(height: 20),
+                      MaterialButton(
+                        onPressed: () {
+                          // context.read<CartBloc>().add(
+                          //     UpdateCartEvent(product: product, isAdd: true));
+                        },
+                        height: 50,
+                        elevation: 0,
+                        splashColor: Styles.buttonSplahColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        color: Styles.buttonColor,
+                        child: Center(
+                          child: Text(
+                            "add_to_order".i18n(),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                      )
                     ],
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    product.description,
-                    style: TextStyle(
-                      height: 1.5,
-                      color: Colors.grey.shade800,
-                      fontSize: 15,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  MaterialButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    height: 50,
-                    elevation: 0,
-                    splashColor: Styles.buttonSplahColor,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    color: Styles.buttonColor,
-                    child: const Center(
-                      child: Text(
-                        "Add to Cart",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    ),
-                  )
-                ],
-              ))
-        ])),
-      ]),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
